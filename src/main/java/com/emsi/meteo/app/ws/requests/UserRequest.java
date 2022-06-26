4cd2785c3de8af861125fadbf07c8c52d29729c7
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-26 15:56:53 +0100

    Pérsonnaliser les Messages d'erreurs du Validation

--- src/main/java/com/emsi/meteo/app/ws/requests/UserRequest.java
+++ src/main/java/com/emsi/meteo/app/ws/requests/UserRequest.java
@@ -5,14 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 public class UserRequest {
 	@NotNull(message = "first name should not be NULL !")
+	@NotBlank
 	@Size(min = 3,message = "first name should be more than 3 caracter")
 	private String firstName;
+	@NotBlank
 	@NotNull(message = "last name should not be NULL !")
 	@Size(min = 3,message = "last name should be more than 3 caracter")
 	private String lastName;
