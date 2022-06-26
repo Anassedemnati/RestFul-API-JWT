dfada61098f2dfbe955e3ee0e0324e10252a144d
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-26 16:11:42 +0100

    Manipuler l'exception du Validation

--- src/main/java/com/emsi/meteo/app/ws/exceptions/AppExceptionHandler.java
+++ src/main/java/com/emsi/meteo/app/ws/exceptions/AppExceptionHandler.java
@@ -4,11 +4,14 @@ import com.emsi.meteo.app.ws.responses.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class AppExceptionHandler {
@@ -23,5 +26,13 @@ public class AppExceptionHandler {
         ErrorMessage errorMessage = new ErrorMessage(new Date(),exception.getMessage());
 
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> HandleMethodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest request){
+        Map<String,String> mapErrors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error->
+            mapErrors.put(error.getField(),error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(mapErrors, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
