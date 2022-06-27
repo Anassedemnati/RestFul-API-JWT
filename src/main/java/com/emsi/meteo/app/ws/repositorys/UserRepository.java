6063507a96bdcd10ecbb4a475eaad11b4f75ef0e
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-27 13:40:51 +0100

    Requete SQL avec Named Parameters

--- src/main/java/com/emsi/meteo/app/ws/repositorys/UserRepository.java
+++ src/main/java/com/emsi/meteo/app/ws/repositorys/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.emsi.meteo.app.ws.entities.UserEntity;
@@ -18,6 +19,14 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
     @Query(value = "SELECT * FROM users",nativeQuery = true)
     Page<UserEntity> findAllUsers(Pageable pageableRequest);
 
-    @Query(value = "SELECT * FROM users u WHERE (u.first_name = ?1 OR u.last_name = ?1) AND u.email_verification_status= ?2",nativeQuery = true)
+  /*  @Query(value = "SELECT * FROM users u WHERE (u.first_name = ?1 OR u.last_name = ?1) AND u.email_verification_status= ?2",nativeQuery = true)
     Page<UserEntity> findByFirstNameAndLastName(Pageable pageableRequest,String kw,int EmailStatus);
+*/
+
+//Named Parameters
+    @Query(value = "SELECT * FROM users u WHERE (u.first_name = :search OR u.last_name = :search) AND u.email_verification_status= :status",nativeQuery = true)
+    Page<UserEntity> findByFirstNameAndLastName(Pageable pageableRequest,@Param("search")String search,@Param("status") int status);
+
+
+
 }
