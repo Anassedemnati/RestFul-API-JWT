7d5890c5f73c724a442651339dcc43e0e6720183
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-28 20:10:26 +0100

    Livrable WAR Archive

--- src/main/java/com/emsi/meteo/app/ws/Application.java
+++ src/main/java/com/emsi/meteo/app/ws/Application.java
@@ -2,11 +2,17 @@ package com.emsi.meteo.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
